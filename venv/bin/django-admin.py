167878cf432e58063ff0aef494c064b3a0e12d0c
kayaleeeee <gayeon71057@gmail.com>
2020-05-02 00:44:02 +0900

    Django project

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kohdongki/Desktop/likelion_vn/Likelion_team_project-master/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
